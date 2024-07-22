367e7a24abca4a5674465d4b10e967994e76c1e2
Yash Kolambekar <41070233+yashkolambekar@users.noreply.github.com>
2024-07-22 16:15:52 +0530

    Update index.php

--- up/index.php
+++ up/index.php
@@ -1,2 +1,4 @@
+<?php
+
 header("Location: https://status.uptime-monitor.io/669e373e53f214cbe72eb35d?_gl=1*7d3h3q*_gcl_au*MjExMTI1MjE0My4xNzIxNjQ0Njc4*_ga*MTc2MjY0MDg0Ny4xNzIxNjQ0Njc4*_ga_Z8TT3FLVJQ*MTcyMTY0NDY3Ny4xLjEuMTcyMTY0NDg0Mi40OS4wLjA.");
 die();
